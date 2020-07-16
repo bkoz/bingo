513aa4173d32050f6a3fc70a35053df758c1c6d5
Bob Kozdemba <bkozdemba@gmail.com>
2020-07-15 22:10:05 -0500

    updated

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# bingo
+# Create bingo cards
+
+## How to
+
+Generate 30 free bingo cards from https://myfreebingocards.com/numbers/1-75/edit and save the 30 links in 
+a text file called `cards.txt`. Create a text file with 30 players called `players.txt.
+
+### Create the card
+
+```
+python bingo.py > index.html
+```
+
+### Run a web server
+
+```
+python -m http.server 5000
+```
+
+### Create a tunnel with ngrok
+
+```
+$ pip install pyngrok
+$ python
+python> ngrok http 5000
+```
+
+
--- bingo.py
+++ bingo.py
@@ -6,10 +6,18 @@
 # Create an html page of players with playerks to bingo cards.
 #
 
+def makePage() :
+  title = "<title>Bingo</title>"
+  header = "<h1>Bingo Cards</h1>"
+  print(title)
+  print(header)
+
+makePage()
+
 players = "players.txt"
 cards = "cards.txt"
 with open(players, 'r') as playerLines:
   with open(cards, 'r') as cardLines:
     for player, card in zip(playerLines, cardLines):
-        s = "<a href=https://"+str.rstrip(card)+">"+str.rstrip(player)+"</a><br>"
+        s = "<h2><a href=https://"+str.rstrip(card)+">"+str.rstrip(player)+"</a></h2>"
         print(s)
